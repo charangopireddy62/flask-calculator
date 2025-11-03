842187e44b06e1514e85cfd4b3fd097f462debaf
Charan Gopireddy <charangopireddy62@gmail.com>
2025-11-03 19:05:07 +0530

    app code changed

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-from flask import Flask, request, jsonify
-import calculator
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🧮 Calculator Flask App is running!"
+    return render_template('index.html', result=None)
 
-@app.route('/add')
-def add_route():
-    a = float(request.args.get('a'))
-    b = float(request.args.get('b'))
-    result = calculator.add(a, b)
-    return jsonify({'result': result})
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+    
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'sub':
+        result = num1 - num2
+    elif operation == 'mul':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    else:
+        result = 'Invalid Operation'
 
-@app.route('/subtract')
-def subtract_route():
-    a = float(request.args.get('a'))
-    b = float(request.args.get('b'))
-    result = calculator.subtract(a, b)
-    return jsonify({'result': result})
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
